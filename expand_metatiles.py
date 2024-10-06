38899d513a962d90b32bd47b90c07959092990f9
TeamAquasHideout <jonathonheddings@gmail.com>
2024-10-06 14:10:30 -0400

    Process border.bin files and update Metatile Labels
    Metatile labels can be updated very easily with a Find and Replace on 0x2 with 0x8 and on 0x3 with 0x9

--- expand_metatiles.py
+++ expand_metatiles.py
@@ -18,12 +18,22 @@ def process_map_data(old_data):
 
         if elevation == 15:
             elevation = 7
-        if metatile_id >= 0x200:
-            metatile_id += 0x600
+        if metatile_id >= 512:
+            metatile_id = metatile_id + (512 * 3)
 
         new_value = metatile_id | ((collision & NEW_COLLISION_MASK) << 12) | ((elevation & NEW_ELEVATION_MASK) << 13)
         new_data.append(new_value)
+    return new_data
 
+def process_border_data(old_data):
+    new_data = []
+    for value in old_data:
+        metatile_id = value & OLD_METATILE_MASK
+
+        if metatile_id >= 512:
+            metatile_id = metatile_id + (512 * 3)
+
+        new_data.append(metatile_id)
     return new_data
 
 def read_map_bin(file_path):
@@ -35,21 +45,30 @@ def write_map_bin(file_path, new_data):
     with open(file_path, "wb") as f:
         f.write(struct.pack("<" + "H" * len(new_data), *new_data))
 
-def find_map_files(base_path="data/layouts"):
+def find_map_files(file_name="map.bin"):
     map_files = []
-    for root, dirs, files in os.walk(base_path):
+    for root, dirs, files in os.walk("data/layouts"):
         for file in files:
-            if file == "map.bin":
+            if file == file_name:
                 map_files.append(os.path.join(root, file))
     return map_files
 
-def process_all_maps(base_path="data/layouts"):
-    map_files = find_map_files(base_path)
+def process_all_maps():
+    map_files = find_map_files("map.bin")
     for map_file in map_files:
         print(f"Processing {map_file}...")
         old_data = read_map_bin(map_file)
         new_data = process_map_data(old_data)
         write_map_bin(map_file, new_data)
 
+def process_all_borders():
+    map_files = find_map_files("border.bin")
+    for map_file in map_files:
+        print(f"Processing {map_file}...")
+        old_data = read_map_bin(map_file)
+        new_data = process_border_data(old_data)
+        write_map_bin(map_file, new_data)
+
 if __name__ == "__main__":
     process_all_maps()
+    process_all_borders()
