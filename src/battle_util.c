c696bfa7e70005655cb19c2e921f67757923d933
c0d3r_ <147667582+coderthegreat2@users.noreply.github.com>
2025-03-23 12:25:40 -0400

    Added gen 9 exp system
    see title

--- src/battle_util.c
+++ src/battle_util.c
@@ -11875,10 +11875,7 @@ bool32 MoveEffectIsGuaranteed(u32 battler, u32 battlerAbility, const struct Addi
 
 bool32 IsGen6ExpShareEnabled(void)
 {
-    if (I_EXP_SHARE_FLAG <= TEMP_FLAGS_END)
-        return FALSE;
-
-    return FlagGet(I_EXP_SHARE_FLAG);
+    return TRUE;
 }
 
 
